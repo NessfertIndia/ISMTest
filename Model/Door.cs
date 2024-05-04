ee63efbceab16627f517bfdd79e136804e079600
Jibin Mathew <jibin.mathew@khl.com>
2024-05-04 23:06:38 +0100

    Add project files.

---
+++ Model/Door.cs
@@ -0,0 +1,18 @@
+﻿namespace ISMTest.Model
+{
+    public enum DoorStateOpen { Open, Closed }
+    public enum DoorStateLocked { Locked, UnLocked }
+    public enum DoorStateActive { Active, Alarmed }
+
+
+
+    public class Door
+    {
+        public Guid Identifier { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DoorStateOpen StateOpen { get; set; }
+        public DoorStateLocked StateLocked { get; set; }
+        public DoorStateActive StateActive { get; set; }
+    }
+}
---
+++ Model/DoorStateFormModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ISMTest.Model
+{
+    public class DoorStateFormModel
+    { 
+        public Guid? SelectedDoorId { get; set; }
+
+        public string SelectedDoorName { get; set; }
+
+        public bool StateOpened { get; set; }
+
+        public bool StateLocked { get; set; }
+
+        public bool StateActive { get; set; }
+    }
+}
